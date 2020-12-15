40a7bde9efeee518760212a4dffaca10ed2d9379
Asa Holland <hollandasa@gmail.com>
2020-12-14 21:35:19 -0800

    Added email copy to clipboard function
    Added email copy function to sidebar and about me page.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,43 +1,59 @@
+// Script modified from eclarrrk on CodePen: https://codepen.io/eclarrrk/pen/ZZywZv
 
-
+// Function to show prompts to user while hovering over email link
 $(document).ready(function() {
 	
 	// Add class to mailto link
 	// Needed to separate the disabling of the default action AND copy email to clipboard
 	$('a[href^=mailto]').addClass('mailto-link');
 
+	// Set up initial variables for mailto class and default prompt strings
 	var mailto = $('.mailto-link');
-	var messageCopy = 'Click to copy email address';
+	var messageCopyPrompt = 'Click or tap to copy email address to clipboard';
 	var messageSuccess = 'Email address copied to clipboard';
 	
+	// Adds the copy prompt span to the item with the mailto class
 	mailto.append('<span class="mailto-message"></span>');
-	$('.mailto-message').append(messageCopy);
+	$('.mailto-message').append(messageCopyPrompt);
 	
-	// Disable opening your email client. yuk.
+	// Disable the default process of opening the email client
 	$('a[href^=mailto]').click(function() {
 		return false; 
 	})
 	
-	// On click, get href and remove 'mailto:' from value
-	// Store email address in a variable.
+	// On click/tap, save the href value of the provided element. 
+	// Remove the 'mailto:' substring from the href value, leaving just the email address
+	// Store this email address in a variable as 'email' using the copyToClipboard function.
 	mailto.click(function() {
 		var href = $(this).attr('href');
 		var email = href.replace('mailto:', '');
 		copyToClipboard(email);
 		$('.mailto-message').empty().append(messageSuccess);
 		setTimeout(function() {
-			$('.mailto-message').empty().append(messageCopy);}, 2000); 
+			$('.mailto-message').empty().append(messageCopyPrompt);}, 2000); 
 	});
 	
 });
 
-// Grabbed this from Stack Overflow.
-// Copies the email variable to clipboard
+// Script modified from walkman on StackOverflow: https://stackoverflow.com/questions/33855641/copy-output-of-a-javascript-variable-to-the-clipboard
+// Copies the provided text to the clipboard
 function copyToClipboard(text) {
-    var dummy = document.createElement("input");
-    document.body.appendChild(dummy);
-    dummy.setAttribute('value', text);
-    dummy.select();
+
+	// Create a new temporary variable on the webpage based on input
+    var temporary = document.createElement("input");
+
+    // add the temporary variable to the body of the document
+    document.body.appendChild(temporary);
+
+    // set the text value of the temporary object  to be that of the provided text
+    temporary.setAttribute('value', text);
+
+    // select the temporary element
+    temporary.select();
+
+    // copy the selected element to the clipboard
     document.execCommand('copy');
-    document.body.removeChild(dummy);
+
+    // remove the temporary element from the document body
+    document.body.removeChild(temporary);
 }
