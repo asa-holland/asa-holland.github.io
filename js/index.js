83c18b19f05c79f7ba62ae147ef6000a6f8b71a1
Asa Holland <asaleholland@gmail.com>
2021-08-30 11:59:07 -0700

    update to Resume section

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// toggles the expansion of the visibility of an iframe
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+
+
+
+    
+
+    console.log("hello from index.js");
